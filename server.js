30caf8abb5f32ee911f76bdad4461f40271ed560
RD1DGM <dominicmariscal78@gmail.com>
2020-08-10 19:26:12 -0400

    updates

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const cors = require('cors')
+const puppeteer = require('puppeteer')
 const app = express()
 
-const PORT = 4000
+const PORT = process.env.PORT || 5555
+const Origin = process.env.Oirigin || '*'
+
+app.use(cors({
+    origin: Origin,
+    methods: ['GET']
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
-app.listen(PORT, () => {
-    console.log(`listening to port: ${PORT}`)
+
+app.get('/product/:productName', (req, res, next) => {
+    console.log(`req.protocol = {{ ${req.protocol} }}\n req.get('host') = {{ ${req.get('host')} }}\n req.originalUrl = {{ ${req.originalUrl} }}\n req.params = {{${JSON.stringify(req.params)}}}`)
+    res.json(req.params)
+})
+
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('error')
+    } else {
+        console.log(`listening to port: ${PORT}`)
+    }
 })
