5ced2051a56c0bb0751e2c5f99138e38606437eb
RD1DGM <dominicmariscal78@gmail.com>
2020-08-13 02:38:00 -0400

    scrapper update

--- utils/scraper.js
+++ utils/scraper.js
@@ -8,12 +8,28 @@ const bestBuyScrapper = async function (item) {
         `https://www.bestbuy.ca/en-ca/search?path=currentoffers0enrchstring%253aOn%2BSale&search=${item}`,
         { waitUntil: 'networkidle2' })
 
+    await page.setViewport({
+        width: 1957,
+        height: 9999
+    });
+
     const productNameSelector = 'div[class*="productItemName_3IZ3c"]'
     const priceSelector = 'div[class*="price_FHDfG"]'
     const linkSelector = 'a[class*="link_3hcyN"]'
+    const imageSelector = 'img[class^="productItemImage_1en8J"]'
+
+    await page.evaluate(() => { window.scrollBy(0, window.innerHeight) })
 
     await page.waitForSelector(`${productNameSelector}`)
     await page.waitForSelector(`${priceSelector}`)
+    await page.waitForSelector(`${linkSelector}`)
+    await page.waitForSelector(`${imageSelector}`)
+
+
+    const images = await page.evaluate((selector) => {
+        const data = Array.from(document.querySelectorAll(`${selector}`), el => el.attributes.src.value)
+        return data
+    }, imageSelector)
 
     const productNames = await page.evaluate((selector) => {
         const data = Array.from(document.querySelectorAll(`${selector}`), el => el.textContent)
@@ -30,13 +46,21 @@ const bestBuyScrapper = async function (item) {
         return data
     }, linkSelector)
 
+
     browser.close()
 
     const products = []
 
     for (let i = 0; i < productNames.length; i++) {
-        products.push({ productName: productNames[i], price: prices[i], link: links[i] })
+        products.push({ productName: productNames[i], price: prices[i], link: links[i], image: images[i] })
     }
+
+    console.log({
+        images: images.length,
+        productNames: productNames.length,
+        prices: prices.length,
+        links: links.length
+    })
 
     return { products }
 }
